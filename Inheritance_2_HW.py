421ad3c5bb2904c890a3b7866b00b2b04eafd5be
mbas1984 <59780585+mbas1984@users.noreply.github.com>
2020-07-20 00:24:32 -0400

    Add files via upload

---
+++ Inheritance_2_HW.py
@@ -0,0 +1,32 @@
+"""
+Week 3 -Inheritance 2
+
+Answer the following questions:
+
+1. What are the parent and child classes here?
+Parent - Spell
+child - Accio,Confundo
+
+2. What are the base and sub-classes?
+base - Spell
+sub-class- Accio,Confundo
+3. What is the output from this code?   Try without running if you can
+Confundo
+Confundo Confundus Charm  Causes the victim to become confused and befuddled
+no parameter
+
+4. When study_spell(Confundo()) executes...what get_description method gets called and why?
+missing parameter
+
+5. The statement print Accio() needs to print ‘This charm summons an object to the caster, potentially over a significant distance’)?
+Write down the code that we need to add and/or change.
+def displayAccio(self):
+    return f"This charm summons an object to the caster, potentially over a significant distance"
+
+
+
+
+
+
+
+"""
